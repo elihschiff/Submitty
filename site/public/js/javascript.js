abaa2fd4b34dd91180285f85ba423224f4a27625
Eli Schiff <eli@elischiff.org>
2019-10-02 17:48:53 -0400

    works with all putlic/js/*.js files that are currently in the repo

---
+++ site/public/js/javascript.js
@@ -0,0 +1,146 @@
+<table id="jslicense-labels1">
+
+        	<tr>
+            <td><a href="/js/admin-gradeable-updates.js">admin-gradeable-updates.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/admin-gradeable-updates.js">admin-gradeable-updates.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/confetti.js">confetti.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/confetti.js">confetti.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/configuration.js">configuration.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/configuration.js">configuration.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/course-settings.js">course-settings.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/course-settings.js">course-settings.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/diff-viewer.js">diff-viewer.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/diff-viewer.js">diff-viewer.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/directory.js">directory.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/directory.js">directory.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/drag-and-drop.js">drag-and-drop.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/drag-and-drop.js">drag-and-drop.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/extensions.js">extensions.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/extensions.js">extensions.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/forum.js">forum.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/forum.js">forum.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/grade-inquiry.js">grade-inquiry.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/grade-inquiry.js">grade-inquiry.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/gradeable-notebook.js">gradeable-notebook.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/gradeable-notebook.js">gradeable-notebook.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/gradeable.js">gradeable.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/gradeable.js">gradeable.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/homepage.js">homepage.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/homepage.js">homepage.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/javascript.js">javascript.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/javascript.js">javascript.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/latedays.js">latedays.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/latedays.js">latedays.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/menu.js">menu.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/menu.js">menu.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/rainbow-customization.js">rainbow-customization.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/rainbow-customization.js">rainbow-customization.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/server.js">server.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/server.js">server.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/simple-grading.js">simple-grading.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/simple-grading.js">simple-grading.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/ta-grading-keymap.js">ta-grading-keymap.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/ta-grading-keymap.js">ta-grading-keymap.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/ta-grading-rubric-conflict.js">ta-grading-rubric-conflict.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/ta-grading-rubric-conflict.js">ta-grading-rubric-conflict.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/ta-grading-rubric.js">ta-grading-rubric.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/ta-grading-rubric.js">ta-grading-rubric.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/ta-grading.js">ta-grading.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/ta-grading.js">ta-grading.js</a></td>
+        	</tr>
+
+        	<tr>
+            <td><a href="/js/userform.js">userform.js</a></td>
+            <td><a href="http://opensource.org/licenses/BSD-3-Clause">BSD 3-Clause License</a></td>
+            <td><a href="https://github.com/Submitty/Submitty/tree/master/site/public/js/userform.js">userform.js</a></td>
+        	</tr>
+    </table>
